c4926644f2a11f2a94d2272002d9376294fe1715
Lucas Simao <Lucas Simao>
2019-07-14 10:31:55 -0300

    config fix

--- src/config/index.js
+++ src/config/index.js
@@ -2,23 +2,20 @@ const env = process.env.NODE_ENV || 'development'
 
 const baseConfig = {
   env,
-  isDev: env === 'development',
-  isTest: env === 'testing',
+  isDev: env == 'development',
+  isTest: env == 'test' ,
   port: 3000,
 }
 
 let envConfig = {}
 
 switch (env) {
-  case 'dev':
   case 'development':
     envConfig = require('./dev')
     break
   case 'test':
-  case 'testing':
     envConfig = require('./testing')
     break
-  case 'prod':
   case 'production':
     envConfig = require('./production')
 }
